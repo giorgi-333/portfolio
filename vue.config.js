4f393c2e7bd87192bd3a28428f996d523901ade6
giorgi-333 <Sachxereze123>
2022-08-15 10:31:25 +0400

    Updated docs for hosting

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,4 @@ const path = require("path");
 module.exports = {
     publicPath: process.env.NODE_ENV === "production" ? "/portfolio/" : "/",
     outputDir: path.resolve(__dirname, "./docs"),
-    // assetsDir: "../../static/SPA"
 };
