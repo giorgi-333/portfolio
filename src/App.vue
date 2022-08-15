9449e2e76952e02783b87bee86da8d4173daf2ed
giorgi-333 <Sachxereze123>
2022-08-15 13:29:53 +0400

    Added fonts

--- src/App.vue
+++ src/App.vue
@@ -3,50 +3,5 @@
 </template>
 
 <style lang="scss">
-
-* {
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-}
-
-html {
-  scroll-behavior: smooth;
-}
-
-body {
-  background: #f2efe9;
-}
-
-h1 {
-  color: rgb(33, 37, 41);
-  font-weight: 500;
-  font-size: 26px;
-}
-
-img {
-  vertical-align: middle;
-}
-
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import "./assets/css/style.scss";
 </style>
